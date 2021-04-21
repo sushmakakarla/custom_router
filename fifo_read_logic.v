565c3b7c810ea8e6883044e810e8559469e8952e
Vladislav Rykov <vladoss@list.ru>
2021-04-21 11:30:55 -0700

    old_dev created

--- fifo_read_logic.v
+++ fifo_read_logic.v
@@ -4,7 +4,7 @@
 `define DEST_ID	1 //< DestID byte index
 
 module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ = 4)
-			(input clk2, rst,
+			(input clk, rst,
 			 input read_port_1_done, read_port_2_done, read_port_3_done,
 			 input [(PTR_SZ-1):0] rq2_wptr,
 			 input [(UWIDTH-1):0] udata,
@@ -48,17 +48,18 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
   reg [(UWIDTH-1):0] dest_id;
   reg [2:0] out_port;
 
-  assign ptr_gray = (rptr >> 1) ^ rptr;
+  assign ptr_gray = rptr;
+  //assign ptr_gray = (rptr >> 1) ^ rptr;
   //assign rq2_wptr[] 
 
-  always @(posedge clk2 or negedge rst)
+  always @(posedge clk or negedge rst)
   begin
     if (!rst) current_state <= IDLE;
     else      current_state <= next_state;
   end
 
   // sequential output
-  always @(posedge clk2 or negedge rst)
+  always @(posedge clk or negedge rst)
   begin
     if (!rst)
     begin
@@ -80,8 +81,16 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       iwrite_en <= 0;
       iread_en <= 0;
 
-      ridx = 0;
-      dest_id = 0;
+      port_1_ridx <= 0;
+      port_1_widx <= 0;
+      port_2_ridx <= 0;
+      port_2_widx <= 0;
+      port_3_ridx <= 0;
+      port_3_widx <= 0;
+
+      ridx <= 0;
+      dest_id <= 0;
+      out_port <= 0;
     //else
       
     end
@@ -96,7 +105,7 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       IDLE: begin
         if (!vrempty) begin
           // read right index from fifo_idx_map
-          iaddr = rq2_wptr;
+          iaddr = rptr;
           iread_en = 1;
           vrptr = (vrptr + 1) % DEPTH;
           next_state = READ_NEXT_IDX;
@@ -114,7 +123,9 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       end
       READ_DST_ID: begin
         // read dest_id
+        uread_en = 0;
         dest_id = udata;
+        next_state = DISPATCH;
       end
       DISPATCH: begin
         out_port = dest_id >= 0 && dest_id <= 127? 0 : dest_id >= 128 && dest_id <= 195? 1 : 2;
@@ -133,18 +144,20 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
             port_3_widx <= (port_3_widx + 1) % DEPTH;
           end
         endcase
+
+        next_state = IDLE;
       end
     endcase
   end
 
   // Port 1 FSM
-  always @(posedge clk2 or negedge rst)
+  always @(posedge clk or negedge rst)
   begin
     if (!rst) port_1_current_state <= PIDLE;
     else      port_1_current_state <= port_1_next_state;
   end
 
-  always @(*)
+  always @(port_1_current_state, port_1_widx, posedge read_port_1_done)
   begin
     port_1_next_state = port_1_current_state;
 
@@ -152,7 +165,7 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       PIDLE: begin
         if (!port_1_empty) begin
           raddr_port_1 = port_1_queue[port_1_ridx];
-          read_port_1_en = 0;
+          read_port_1_en = 1;
           port_1_next_state = PBUSY;
         end else
           port_1_next_state = PIDLE;
@@ -177,13 +190,13 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
   end
 
   // Port 2 FSM
-  always @(posedge clk2 or negedge rst)
+  always @(posedge clk or negedge rst)
   begin
     if (!rst) port_2_current_state <= PIDLE;
     else      port_2_current_state <= port_2_next_state;
   end
 
-  always @(*)
+  always @(port_2_current_state, port_2_widx, posedge read_port_2_done)
   begin
     port_2_next_state = port_2_current_state;
 
@@ -191,7 +204,7 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       PIDLE: begin
         if (!port_2_empty) begin
           raddr_port_2 = port_2_queue[port_2_ridx];
-          read_port_2_en = 0;
+          read_port_2_en = 1;
           port_2_next_state = PBUSY;
         end else
           port_2_next_state = PIDLE;
@@ -203,10 +216,12 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
           idata = port_2_queue[port_2_ridx];
           iwrite_en = 1;
           port_2_next_state = PDONE;
-        end else
+        end else begin
           port_2_next_state = PBUSY;
+        end
       end
       PDONE: begin
+        $display("here done");
         iwrite_en = 0;
         rptr = (rptr + 1) % DEPTH;
         port_2_ridx = (port_2_ridx + 1) % DEPTH;
@@ -216,13 +231,13 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
   end
 
   // Port 3 FSM
-  always @(posedge clk2 or negedge rst)
+  always @(posedge clk or negedge rst)
   begin
     if (!rst) port_3_current_state <= IDLE;
     else      port_3_current_state <= port_3_next_state;
   end
 
-  always @(*)
+  always @(port_3_current_state, port_3_widx, posedge read_port_3_done)
   begin
     port_3_next_state = port_3_current_state;
 
@@ -230,7 +245,7 @@ module fifo_read_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2, PTR_IN_SZ 
       PIDLE: begin
         if (!port_3_empty) begin
           raddr_port_3 = port_3_queue[port_3_ridx];
-          read_port_3_en = 0;
+          read_port_3_en = 1;
           port_3_next_state = PBUSY;
         end else
           port_3_next_state = PIDLE;
