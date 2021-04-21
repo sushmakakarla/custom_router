74dfdef2a3a341e592b36f42256152fc09e24540
Vladislav Rykov <vladoss@list.ru>
2021-04-21 13:04:14 -0700

    write logic done

--- fifo_write_logic.v
+++ fifo_write_logic.v
@@ -15,33 +15,63 @@ module fifo_write_logic #(parameter DEPTH = 3, UWIDTH = 8, PTR_SZ = 2)
   localparam IDLE = 2'b00, WRITE = 2'b01, FULL = 2'b10;
   reg [1:0] current_state, next_state;
   
-  reg [(PTR_SZ-1):0] raddr;
-  assign waddr_gray = (waddr >> 1) ^ waddr;
+  reg wfull_tmp, write_en_tmp;
+  reg [(PTR_SZ-1):0] raddr, waddr_tmp;
+  assign [(PTR_SZ-1):0] waddr_gray_tmp = (waddr_tmp >> 1) ^ waddr_tmp;
 
+  // FSM sequential block
   always @(posedge clk or negedge rst)
   begin
     if (!rst) current_state <= IDLE;
     else      current_state <= next_state;
   end
 
-  always @(*) 
+  // FSM combinational block
+  always @(current_state or wfull_tmp)
   begin
     next_state = current_state;
-    
+    write_en_tmp = 0;
+
     case (current_state)
       IDLE: begin
-        if (!wfull) next_state = WRITE;
+        write_en_tmp = 0;
+        if (!wfull_tmp) next_state = WRITE;
         else        next_state = IDLE;
       end
       WRITE: begin
-        
+        write_en_tmp = 1;
+        if (wfull_tmp)  next_state = FULL;
+        else        next_state = WRITE;
       end
       FULL: begin
- 
+        write_en = 0;
+        if (!wfull_tmp) next_state = WRITE;
+        else        next_state = FULL;
       end
   end
 
+  // sequential block
+  always @(posedge clk or negedge rst)
+  begin
+    if (!rst) begin
+      write_en <= 0;
+      wfull <= 0;
+      waddr <= 0;
+      waddr_gray <= 0;
+    end else begin
+      write_en <= write_en_tmp;
+      wfull <= wfull_tmp;
+      waddr <= waddr_tmp;
+      waddr_gray <= waddr_gray_tmp;
+    end
+  end
 
+  // combinational blocks
+  always @(*)
+  begin
+    wfull_tmp = ((waddr_tmp + 1) % DEPTH) == raddr;
+    if (winc && !wfull_tmp) waddr_tmp = (waddr_tmp + 1) % DEPTH;
+  end
 
   always @(rq2_raddr)
     for (i = 0; i < PTR_SZ; i=i+1)
